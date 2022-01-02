53b2e9fd523c898447debf0e88e94af09a2f0f42
feirou <823246785@qq.com>
2022-01-02 17:42:39 +0800

    校园网更新，原有爬虫失效
    Update了一版

--- 校园网自动登录.py
+++ 校园网自动登录.py
@@ -1,5 +1,6 @@
 # author: zephyr ji
 # time: 2020/10/01
+# Update By donald Tang :2022_1_2
 # pip install selenium
 # download chromedriver：http://chromedriver.storage.googleapis.com/index.html
 # https://www.cnblogs.com/lfri/p/10542797.html
@@ -18,12 +19,13 @@ def login(username, password):
     option = webdriver.ChromeOptions()
     option.add_argument("--headless")
     
-    driver = webdriver.Chrome(executable_path=chrome_driver,chrome_options=option)
+    # driver = webdriver.Chrome(executable_path=chrome_driver,chrome_options=option)
+    driver = webdriver.Chrome(executable_path=chrome_driver)
     driver.get(url)
     print(driver.title)
-    name_input = driver.find_element_by_id('VipDefaultAccount')  # 找到用户名的框框
-    pass_input = driver.find_element_by_id('VipDefaultPassword')  # 找到输入密码的框框
-    login_button = driver.find_element_by_xpath('/html/body/div/div/div[2]/form[2]/div[2]/input[1]')  # 找到登录按钮
+    name_input = driver.find_element_by_css_selector('[placeholder ="请输入账号"]')  # 找到用户名的框框
+    pass_input = driver.find_element_by_css_selector('[placeholder ="请输入密码"]')  # 找到输入密码的框框
+    login_button = driver.find_element_by_css_selector('[value = " 登 录（Login）"]')  # 找到登录按钮
     # reset_button = driver.find_element_by_id('VipResetButton')  # 找到reset按钮
 
     name_input.clear()
@@ -36,7 +38,6 @@ def login(username, password):
 
     time.sleep(10)
     print(driver.title)
-
     driver.close()
 
 def canConnect():
@@ -55,6 +56,7 @@ def canConnect():
 if __name__ == "__main__":
     user = "322159"  # 输入账号
     pw = "ta27365363PE"  # 输入密码
+
     while True:
         can_connect = canConnect()
         getCurrentTime = time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime(time.time()))
